775884269fe204719aae15fd857724994a20f3f3
webdevyoo <yjk7454@naver.com>
2025-02-17 00:05:38 +0900

    feat: `LifeCycle 01` - beforeCreate, created 훅 예제 코드 추가

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,12 @@ export default {
     return {
       count: 0
     };
+  },
+  beforeCreate() {
+    console.log("LifeCycle is beforeCrete", this.count) //  컴포넌트가 생성되기 전 이므로 undefined 출력
+  },
+  created() {
+    console.log("LifeCycle is creted", this.count) // 컴포넌트가 생성된 후 초기화 진행되므로 0 출력
   }
 };
 </script>
