519dcf1bcdadff0ae8db16e66fd0b22a37964acf
webdevyoo <yjk7454@naver.com>
2025-02-21 00:11:03 +0900

    feat: `VHtml.vue` v-Html 디렉티브 예제 컴포넌트 추가

---
+++ src/components/vue2/VHtml.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ rawHtml }}</div>
+  <div>{{ rawHtml2 }}</div>
+  <h1 v-html="rawHtml2"></h1>
+</template>
+<script>
+export default {
+  name: 'VHtml',
+  data() {
+    return {
+      rawHtml: '이것은 텍스트 입니다.',
+      rawHtml2: '<span style="color: red;">이것은 빨간색 텍스트 입니다.</span>'
+    }
+  }
+}
+</script>
--- src/components/vue2/Vue2.vue
+++ src/components/vue2/Vue2.vue
@@ -1,13 +1,16 @@
 <template>
   <LifeCycle/>
+  <VHtml/>
 </template>
 
 <script>
 import LifeCycle from './LifeCycle.vue';
+import VHtml from './VHtml.vue';
 export default {
   name: 'Vue2',
   components: {
-    LifeCycle
+    LifeCycle,
+    VHtml
   }
   
 }
