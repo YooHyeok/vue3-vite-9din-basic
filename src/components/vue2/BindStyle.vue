95d483d4359c9725a91a500ecaf19988cf62c019
webdevyoo <yjk7454@naver.com>
2025-02-21 00:12:39 +0900

    feat: `BindClass.vue` v-bind 디렉티브 class 속성 바인드 예제 컴포넌트 추가

---
+++ src/components/vue2/BindStyle.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>[스타일 바인딩 테스트]</h1>
+  <h3 style="color: red; font-size: 24px">인라인스타일 바인딩</h3>
+  <h3 :style="{color: '#888888', fontSize: 48 + 'px'}">Vue.JS스타일 바인딩</h3>
+  <h3 :style="{color: fontColor, fontSize: fontSize + 'px'}">Vue.JS스타일 바인딩 - 변수활용</h3>
+</template>
+<script>
+export default {
+  name: 'VHtml',
+  data() {
+    return {
+      fontColor: '#999999',
+      fontSize: 36
+    }
+  },
+  methods: {
+    change() {
+      this.isActive = !this.isActive;
+    }
+  }
+}
+</script>
+<style scoped>
+h2.active {
+  color: green;
+}
+</style>
--- src/components/vue2/Vue2.vue
+++ src/components/vue2/Vue2.vue
@@ -2,18 +2,21 @@
   <LifeCycle/>
   <VHtml/>
   <BindClass/>
+  <BindStyle/>
 </template>
 
 <script>
 import LifeCycle from './LifeCycle.vue';
 import VHtml from './VHtml.vue';
 import BindClass from './BindClass.vue';
+import BindStyle from './BindStyle.vue';
 export default {
   name: 'Vue2',
   components: {
     LifeCycle,
     VHtml,
-    BindClass
+    BindClass,
+    BindStyle
   }
   
 }
